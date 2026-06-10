f2a38869fa0f3eda4b6dab32b9db7afd8ea73ce9
Helen Silva <helen_silva404@hotmail.com>
2026-06-09 22:58:50 -0300

    feat: adiciona componente likert e strategies

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,18 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { LikertGeral } from './components/likert-geral/likert-geral';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, LikertGeral],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, MatSelectModule, MatFormFieldModule, MatInputModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
